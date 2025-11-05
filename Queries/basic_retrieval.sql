10e21396d2d6b4497846bc4d2a0f0bbcd1b07f97
Seif Alaa <seif.alaa1231@gmail.com>
2025-11-05 11:28:53 +0200

    Implement allEmployeeProfiles View

---
+++ Queries/basic_retrieval.sql
@@ -0,0 +1,15 @@
+CREATE VIEW allEmployeeProfiles AS
+SELECT 
+    employee_ID,
+    first_name,
+    last_name,
+    gender,
+    email,
+    address,
+    years_of_experience,
+    official_day_off,
+    type_of_contract,
+    employment_status,
+    annual_balance,
+    accidental_balance
+FROM Employee;
