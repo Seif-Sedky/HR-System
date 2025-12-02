d8a9f6d208640d7e99b6a06ee4f8335b2b6e03eb
Yootooo <yokimaxwel@gmail.com>
2025-12-02 14:58:30 +0200

    Feat: Added department statistics page

---
+++ Milestone3/Pages/Admin/DepartmentStats.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data; // For DataTable
+using System.Threading.Tasks;
+
+namespace Milestone3.Pages.Admin
+{
+    public class DepartmentStatsModel : PageModel
+    {
+        private readonly Database _db;
+
+        // Container for the data
+        public DataTable Stats { get; set; }
+
+        public DepartmentStatsModel(Database db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+            // We select from the VIEW your team created
+            string query = "SELECT * FROM NoEmployeeDept";
+
+            Stats = await _db.ExecuteQuery(query);
+        }
+    }
+}
