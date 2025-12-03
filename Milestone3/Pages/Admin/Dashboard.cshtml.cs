ef244ab0f9cc43468a5882f0b3ac580e41fcb43f
Yootooo <yokimaxwel@gmail.com>
2025-12-03 12:53:27 +0200

    Feat: Add the admin dashboard

---
+++ Milestone3/Pages/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Milestone3.Pages.Admin
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
