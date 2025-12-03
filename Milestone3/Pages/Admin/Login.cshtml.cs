f140ef19d58c496268ef59003252257fd1774c3e
Yootooo <yokimaxwel@gmail.com>
2025-12-03 12:52:13 +0200

    Enhanced the login page

--- Milestone3/Pages/Admin/Login.cshtml.cs
+++ Milestone3/Pages/Admin/Login.cshtml.cs
@@ -15,22 +15,19 @@ namespace Milestone3.Pages.Admin
 
         public void OnGet()
         {
-            // Nothing needed here yet
+         
         }
 
-        // 2. This is the "Mitt" that catches the form submission
-        // The parameter names (adminId, password) MUST match the name="..." in your HTML
+        
         public IActionResult OnPost(string adminId, string password)
         {
-            // 3. The Hardcoded Check (Per Project Requirement)
+          
             if (adminId == "Yoo" && password == "Yoo is cool")
             {
-                // SUCCESS: Redirect to the Homepage (or wherever you want them to go)
-                return RedirectToPage("/Index");
+                return RedirectToPage("/Admin/Dashboard");
             }
             else
             {
-                // FAILURE: Set an error message and stay on this page
                 Message = "Invalid Admin ID or Password";
                 return Page();
             }
