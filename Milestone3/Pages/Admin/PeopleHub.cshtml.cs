f547d97eda132bb6c984e3c9b09f6d4190171c22
Yootooo <yokimaxwel@gmail.com>
2025-12-04 03:58:06 +0200

    Created the PeopleHub, where ViewProfiles, DepartmentStats, EmployeePerformance, ReplaceEmployee, UpdateEmployeeStatus where merged in a single clean user friendly file

---
+++ Milestone3/Pages/Admin/PeopleHub.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Milestone3.Pages.Admin
+{
+    public class PeopleHubModel : PageModel
+    {
+        private readonly Database _db;
+
+        public DataTable Employees { get; set; }
+        public DataTable DepartmentStats { get; set; }
+        public DataTable PerformanceRecords { get; set; }
+
+        public PeopleHubModel(Database db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+            string empQuery = "SELECT * FROM Employee";
+            Employees = await _db.ExecuteQuery(empQuery);
+
+            string statsQuery = "SELECT * FROM NoEmployeeDept";
+            DepartmentStats = await _db.ExecuteQuery(statsQuery);
+
+            string perfQuery = "SELECT * FROM allPerformance";
+            PerformanceRecords = await _db.ExecuteQuery(perfQuery);
+        }
+    }
+}
